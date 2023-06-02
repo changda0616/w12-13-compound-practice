82e173f26b5adca23e38b800c32608e434f3c2bd
ChadZ <changda0616@gmail.com>
2023-06-03 01:43:31 +0800

    feat: deploy compound

--- script/DeployComp.s.sol
+++ script/DeployComp.s.sol
@@ -2,25 +2,81 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+
 import "compound-protocol/contracts/Unitroller.sol";
 import "compound-protocol/contracts/Comptroller.sol";
+import "compound-protocol/contracts/ComptrollerInterface.sol";
+
+import "compound-protocol/contracts/CErc20Delegate.sol";
+import "compound-protocol/contracts/CErc20Delegator.sol";
+
+import "compound-protocol/contracts/InterestRateModel.sol";
+import "compound-protocol/contracts/SimplePriceOracle.sol";
+import "compound-protocol/contracts/WhitePaperInterestRateModel.sol";
+
+import "openzeppelin/token/ERC20/ERC20.sol";
 
 contract DeployComp is Script {
-    function setUp() public {
-        
-    }
+    function setUp() public {}
 
     function run() public {
-        uint256 key = vm.envUint('wallet_key');
+        uint256 key = vm.envUint("wallet_key");
+        address admin = vm.envAddress("wallet");
         vm.startBroadcast(key);
+
+        // propose controller and unitroller (proxy)
         Unitroller unitroller = new Unitroller();
         Comptroller comptroller = new Comptroller();
         Comptroller unitrollerProxy = Comptroller(address(unitroller));
-        
-        // prepare oracle
+
+        // set implementaton of unitroller
+        unitroller._setPendingImplementation(address(comptroller));
+        // unitroller accept comptroller
+        comptroller._become(unitroller);
+
+        // set close factor at 60%
+        unitrollerProxy._setCloseFactor(0.6 * 1e18);
+        // set liquidation incentive at 1.08
+        unitrollerProxy._setLiquidationIncentive(1.08 * 1e18);
+        // set prepare oracle
         SimplePriceOracle priceOracle = new SimplePriceOracle();
-        
-        
+        unitrollerProxy._setPriceOracle(priceOracle);
+
+        // prepare InterestRateModel
+        WhitePaperInterestRateModel whitePaperInterestRateModel = new WhitePaperInterestRateModel(
+                0,
+                0
+            );
+
+        // prepare undelying ERC20 Token
+        ERC20 Sai = new ERC20("Sai test", "SAI");
+
+        // prepare token implementation, CErc20Delegate
+        CErc20Delegate cErc20Delegate = new CErc20Delegate();
+        // prepare token proxy, CErc20Delegator
+        CErc20Delegator cSai = new CErc20Delegator(
+            address(Sai),
+            ComptrollerInterface(address(unitroller)),
+            InterestRateModel(address(whitePaperInterestRateModel)),
+            1, // rate 10 ** 18 / 10 ** 18
+            "Compound Sai",
+            "cSAI",
+            18,
+            payable(admin),
+            address(cErc20Delegate),
+            new bytes(0x01)
+        );
+
+        cSai._setReserveFactor(0.1 * 1e18);
+
+        // Add cSai to unitroller's markets map
+        unitrollerProxy._supportMarket(CToken(address(cSai)));
+
+        // The borrower can only borrow 70% of the collateral value
+        unitrollerProxy._setCollateralFactor(CToken(address(cSai)), 0.7 * 1e18);
+
+        priceOracle.setUnderlyingPrice(CToken(address(cSai)), 1e18);
+
         vm.stopBroadcast();
     }
 }
